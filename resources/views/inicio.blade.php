0cc767be954112f2425164995c217eba4921c478
oscarbz <oscarbz0609@gmail.com>
2022-08-05 20:41:40 -0400

    mensaje

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Inicio</title>
+	<h1>Hola mundo</h1>
+</head>
+<body>
+
+</body>
+</html>
